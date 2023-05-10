cef321f5d7a2f3a1a84d7faa22a9d5e81b0a0a12
palmbreeze <iamfranciscoperez@gmail.com>
2023-05-09 21:53:34 -0500

    Applications view

--- app/models/applicationsModel.js
+++ app/models/applicationsModel.js
@@ -6,21 +6,21 @@ exports.readAll = async function (req) {
     try {
         let query = `
         SELECT 
-            app.id AS application_id,
-            jp.name AS job_position_name,
+            app.id,
+            app.name AS job_position,
+            cm.name AS company_name, 
             d.name AS domain_name,
-            w.name AS website_name,
-            c.name AS country_name,
+            app.description,
             app.is_remote,
-            avs.status_id,
+            c.name AS country_name,
             s.name AS status_name,
             avs.date,
-            avs.next_date
+            avs.next_date,
+            app.link 
         FROM 
             application app
-            INNER JOIN job_position jp ON app.job_position_id = jp.id
+            INNER JOIN company cm ON app.company_id = cm.id
             INNER JOIN domain d ON app.domain_id = d.id
-            INNER JOIN website w ON app.found_in = w.id
             INNER JOIN country c ON app.country_id = c.id
             INNER JOIN (
                 SELECT application_id, MAX(date) AS max_date
